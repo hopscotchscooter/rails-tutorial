9a08490164e526e1535909353e6d3d37bf4d99cd
hopscotchscooter <dvlewicki@gmail.com>
2014-01-23 19:58:13 +0000

    Complete

--- app/controllers/posts_controller.rb
+++ app/controllers/posts_controller.rb
@@ -1,4 +1,11 @@
 class PostsController < ApplicationController
+  
+  http_basic_authenticate_with name: "user", password: "secret", except: [:index, :show]
+  
+  def index
+    @posts = Post.all
+  end
+  
   def new
     @post = Post.new
   end
